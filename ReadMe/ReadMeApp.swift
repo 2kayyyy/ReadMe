dbdbc7d3de98f075b50057ef674ee950e5e31f27
Aryan Kafle <aryankafle004@gmail.com>
2022-05-01 15:46:56 +0545

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Aryan Kafle on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
